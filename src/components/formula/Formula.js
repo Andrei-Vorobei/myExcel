e8e78509721d149ee56f5bef97c835deae07c138
Andrei <marchenko_ad@mail.ru>
2020-07-21 20:40:52 +0300

    added changes

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,27 +1,46 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
 
 export class Formula extends ExcelComponent {
 	static className = 'excel__formula'
 
-	constructor($root) {
+	constructor($root, options) {
 		super($root, {
 			name: 'Formula',
-			listeners: ['input', 'click']
+			listeners: ['input', 'keydown'],
+			...options
 		});
 	}
 
 	toHTML() {
 		return `
 			<div class="info">fx</div>
-			<div class="input" contenteditable spellcheck="false"></div>
+			<div id="formula" class="input" contenteditable spellcheck="false"></div>
 		`;
 	}
 
-	onInput(event) {
-		console.log('Formula: onInput', event.target.textContent.trim());
+	init() {
+		super.init();
+
+		this.$formula = this.$root.find('#formula');
+
+		this.$on('table:input', text => {
+			this.$formula.text(text);
+		});
+
+		this.$on('table:select', text => {
+			this.$formula.text(text);
+		});
 	}
 
-	onClick(event) {
-		console.log('Formula: onClick', event.target);
+	onInput(event) {
+		this.$emit('formula:input', $(event.target).text());
+	}
+
+	onKeydown(event) {
+		if (event.key === 'Enter' || event.key === 'Tab') {
+			event.preventDefault();
+			this.$emit('formula:done');
+		}
 	}
 }
